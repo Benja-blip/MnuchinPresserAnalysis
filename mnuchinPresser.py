a5492f2285223387e508253a450e6f4d8bcbb081
Ben Clark <54931430+Benja-blip@users.noreply.github.com>
2020-03-17 14:50:52 -0400

    Add files via upload

---
+++ mnuchinPresser.py
@@ -0,0 +1,50 @@
+import mysql.connector
+from mysql.connector import Error as err
+
+
+def mnuchinPresser():
+
+    cnx = mysql.connector.connect(user='root', password='Sergen1ver2', host='127.0.0.1', database='StockPipeline')
+    cnx.connect()
+    cursor = cnx.cursor()
+
+    print('')
+    print('***************************The weak***************************')
+    print('')
+
+    cursor.execute(
+        "SELECT * FROM `1pmStocks` t1 WHERE NOT EXISTS (SELECT t2.symbol FROM `11amStocks` t2 WHERE t1.symbol = t2.symbol)")
+    the_weak_sql = cursor.fetchall()
+    the_weak_list = []
+    for row in the_weak_sql:
+        the_weak_list.append(row[0])
+
+    print(the_weak_list)
+
+    print('')
+    print('***************************The strong***************************')
+    print('')
+
+    cursor.execute("SELECT * FROM 11amStocks JOIN 1pmStocks ON 11amStocks.symbol = 1pmStocks.symbol")
+    the_strong_sql = cursor.fetchall()
+    the_strong_list = []
+    for row in the_strong_sql:
+        the_strong_list.append(row[0])
+
+    print(the_strong_list)
+
+    print('')
+    print('***************************The strange***************************')
+    print('')
+
+    cursor.execute(
+        "SELECT * FROM `11amStocks` t1 WHERE NOT EXISTS (SELECT t2.symbol FROM `1pmStocks` t2 WHERE t1.symbol = t2.symbol)")
+    the_strange_sql = cursor.fetchall()
+    the_strange_list = []
+    for row in the_strange_sql:
+        the_strange_list.append(row[0])
+
+    print(the_strange_list)
+
+
+mnuchinPresser()
---
+++ usEquitiesImport.py
@@ -0,0 +1,73 @@
+import alpaca_trade_api as tradeapi
+import mysql.connector
+from mysql.connector import Error as err
+
+
+chosen_ones = []
+
+def get_tickers():
+
+    base_url = 'https://paper-api.alpaca.markets'
+    api_key_id = 'PKD01L4TQNX03S47FE22'
+    api_secret = 'nXhSyLXp8eyEgvGrans01vy3h69XiXiJ4SOIjZcs'
+
+    api = tradeapi.REST(
+        base_url=base_url,
+        key_id=api_key_id,
+        secret_key=api_secret
+    )
+
+    min_share_price = 5
+    max_share_price = 15
+
+    print('Getting current ticker data...')
+    tickers = api.polygon.all_tickers()
+    print('Success.')
+    assets = api.list_assets()
+    symbols = [asset.symbol for asset in assets if asset.tradable]
+    for ticker in tickers:
+        if (ticker.ticker in symbols and
+            ticker.lastTrade['p'] >= min_share_price and
+            ticker.lastTrade['p'] <= max_share_price and
+            ticker.todaysChangePerc >= 1.0
+            ):
+            chosen_ones.append(ticker)
+        else:
+            pass
+
+    database_insert()
+
+
+def database_insert():
+
+    cnx = mysql.connector.connect(user='root', password='Sergen1ver2', host='127.0.0.1', database='StockPipeline')
+    cnx.connect()
+    cursor = cnx.cursor()
+
+    try:
+        cursor.execute("CREATE TABLE 1pmStocks (symbol CHAR(5) NOT NULL, prevDayClose DECIMAL(6,4) NOT NULL, prevDayHigh DECIMAL(6,4) NOT NULL, prevDayLow DECIMAL(6,4) NOT NULL, lastTrade DECIMAL(6,4) NOT NULL, todaysChangePerc DECIMAL(6,4) NOT NULL);")
+        print("created")
+    except err:
+        print("err")
+
+    print(chosen_ones)
+    print(len(chosen_ones))
+
+    for ticker in chosen_ones:
+        print(ticker.ticker)
+        print(ticker.prevDay['c'])
+        value = (ticker.ticker, ticker.prevDay['c'], ticker.prevDay['h'], ticker.prevDay['l'], ticker.lastTrade['p'],
+                ticker.todaysChangePerc)
+        query = 'INSERT INTO 1pmStocks (symbol, prevDayClose, prevDayHigh, prevDayLow, lastTrade, todaysChangePerc) VALUES (%s, %s, %s, %s, %s, %s)'
+        try:
+            cursor.execute(query, value)
+            print("db appended")
+            print(value)
+        except ValueError:
+            print(value)
+            print("Value error")
+
+    cnx.commit()
+
+
+get_tickers()
